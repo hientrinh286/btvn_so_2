a8cab6b55d2021a19d77501ab04f095b3e0041e4
hientrinh286 <hientrinh286@gmail.com>
2021-05-15 15:50:28 +0700

    update tinh diem trung binh

---
+++ 2_tinh_diem_trung_binh/tinh_diem_trung_binh.js
@@ -0,0 +1,5 @@
+let diemtoan = +prompt("Nhập điểm Toán:");
+let diemly = +prompt("Nhập điểm Lý:");
+let diemhoa = +prompt("Nhập điểm Hóa:");
+diemtb = (diemtoan+diemly+diemhoa)/3;
+alert("Điểm Trung Bình của bạn là: "+diemtb);
